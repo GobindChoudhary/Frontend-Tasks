e119d94ebfe2367708c5dd13f7ea44c13f354d05
GobindChoudhary <chaudharygobind24@gmail.com>
2025-11-17 22:48:16 +0530

    implemented infinity scroll using intersection observer

--- infiniteScroll/src/App.jsx
+++ infiniteScroll/src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 const App = () => {
   const [data, setData] = useState([...new Array(20)]);
   const [loading, setLoading] = useState(false);
-  const THRESHOLD = 5;
+
+  const refList = useRef([]);
 
   const fetchdata = () => {
     setLoading(true);
@@ -13,24 +14,34 @@ const App = () => {
     }, 2000);
   };
 
-  const handleScroll = (e) => {
-    const clientHeight = e.target.clientHeight;
-    const scrollTop = e.target.scrollTop;
-    const scrollHeight = e.target.scrollHeight;
-    const remaingScroll = scrollHeight - (clientHeight + scrollTop);
-    if (remaingScroll < THRESHOLD) {
-      fetchdata();
-    }
-  };
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        observer.unobserve(entries[0].target);
+        fetchdata();
+      }
+    });
 
-  useEffect(() => {});
+    const lastElement = refList.current[refList.current.length - 1];
+    observer.observe(lastElement);
+
+    return () => {
+      // observer.unobserve(lastElement);
+      observer.disconnect();
+    };
+  }, [data.length]);
+
   return (
     <div className="container">
       <h1>Infinity Scroll</h1>
-      <div onScroll={handleScroll} className="box-container">
+      <div className="box-container">
         {data.map((item, i) => {
           return (
-            <div key={i} className="numbers">
+            <div
+              ref={(el) => (refList.current[i] = el)}
+              key={i}
+              className="numbers"
+            >
               {i}
             </div>
           );
